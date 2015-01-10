038e5775389ec070ffea9f583a4f84752b277d99
Claudia Cuevas <omordn@gmail.com>
2015-01-09 19:43:06 -0800

    updating calculates and output--still need monthly numbers

--- eventHandlerScript.js
+++ eventHandlerScript.js
@@ -7,40 +7,47 @@ function Locations(name, minTraffic, maxTraffic) {
 	this.orderPerEntrant = 2.9;
 	this.orderPerHour = 0;
   this.dailyDonutsNeeded = 0;
+  this.weeklyDonutsNeeded = 0;
+  this.monthlyDonutsNeeded = 0;
 	//this function calculates the donuts sold by the hour depending on the traffic
 	this.donutsSold = function() {
 		this.orderPerHour += ((((Math.random() * (this.maxTraffic - this.minTraffic)) + this.minTraffic) * this.entering * this.orderPerEntrant));
-	};
-
-	this.monthTotal = function () {
-		var total = 0;
-		for (var  i = 0; i < 308; i++) {
-			var eachHour = this.donutsSold ();
-			total += eachHour;
-			return total;
-		}
-		var weekAverage = total / 4;
-		var dailyAverage = total / 28; 
 	};
 	//this function returns the report of each location with the # of donuts sold
   this.dailyDonuts = function() {
     this.dailyDonutsNeeded += this.orderPerHour * 11;
     return this.dailyDonutsNeeded;
   };
+	this.weeklyTotal = function() {
+		this.weeklyDonutsNeeded += (this.dailyDonutsNeeded * 7);
+		return this.weeklyDonutsNeeded;
+	/*this.monthlyTotal = function() {
+		this.monthlyDonutsNeeded += (this.weeklyDonutsNeeded * 4);
+		return this.monthlyDonutsNeeded;*/
+	};
+	/*var total = 0;
+	for (var  i = 0; i < 308; i++) {
+		var eachHour = this.donutsSold();
+		total += eachHour;
+		return total;*/
+
+	/*var weekAverage = total / 4;
+	var dailyAverage = total / 28;*/
+
 	this.report = function() {
 		/*console.log(this.name + " has " + Math.floor(this.orderPerHour) + " donuts per hour.");
     console.log("Total for the day: " + Math.floor(this.dailyDonuts()));*/
-    var xx = this.dailyDonuts();
-    return (this.name + this.orderPerHour + xx);
+    var daily = this.dailyDonuts();
+    var weekly = this.weeklyTotal();
+    //var monthly = this.monthlyTotal();
+    var communication = (this.name + ' is estimated to sell ' + Math.floor(this.orderPerHour) + ' donuts per hour, ' + Math.floor(daily) + ' donuts per day, ' + Math.floor(weekly) + ' weekly, and ' /*+ Math.floor(monthly) + ' donuts per month.')*/);
+    return communication;
+    /*return (this.name + "is estimated to sell " this.orderPerHour +  " per Hour " + xx);*/
 
     /*console.log(this.total);*/
 	};
 }
 
-/* var xxx = new Locations("Downtown", 4, 5);
-xxx.monthTotal();
-xxx.dailyDonuts();
-xxx.report(); */
 var button = document.getElementById('submit');
 var summary = document.getElementById('summary');
 
@@ -50,7 +57,7 @@ function userSubmit() {
 	var max = document.getElementById('max-traffic').value;
 	var userStoreSubmit = new Locations(location, min, max);
 	userStoreSubmit.donutsSold();
-	var msg = '<h2>This is to test our output with Per Hour & Total Day: </h2>';
+	var msg = '<h2>Projections Are: </h2>';
 	msg += userStoreSubmit.report();
 	summary.innerHTML = msg; 
 }
